73008a62412e2c29daea0c94a745d02c4bef7062
WisDev <71902497+igorwisniewski@users.noreply.github.com>
2023-04-24 12:49:59 +0200

    Update Uczen.java

--- src/Uczen.java
+++ src/Uczen.java
@@ -1,9 +1,36 @@
+/**
+
+ <klasa uczen zawiera:
+
+ o dwa stałe pola znakowe imie i nazwisko,
+
+ o pole całkowitoliczbowe id, które generowane jest automatycznie przy użyciu mechanizmu autoinkrementacji (wykorzystaj pomocniczo zmienną statyczną)
+
+ o konstruktor parametryczny oraz konstruktor kopiujący (tworzący obiekt na podstawie innego obiektu klasy uczeń, kopiowaniu podlegają tylko i wyłącznie pola imie oraz nazwisko)
+
+ o w konstruktorach pole id uzupełniane jest automatycznie
+
+ o metody get dla pól imię i nazwisko
+
+ o prawidłowo zaimplementowane metody equals oraz hashCode, przyjmij założenie, że da obiekty są sobie równe tylko wtedy, kiedy ich id jest równe
+
+ o metodę toString>
+
+ <imie> : <String>, <pole na imie>
+ <nazwisko> : <String>, <pole na Nazwisko>
+ <id> : <Integer>, <pole na id>
+
+ @author: <Igor> <Wisniewski>
+
+ **/
+
+
 import java.util.Objects;
 
 public class Uczen {
     public String imie, nazwisko;
     public Integer id;
-    private Integer i;
+
 
     public Uczen(String imie, String nazwisko ) {
         this.imie = imie;
