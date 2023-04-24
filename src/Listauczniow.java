872f60bf071c60da498d3945e2e54889ea1ca2cb
WisDev <71902497+igorwisniewski@users.noreply.github.com>
2023-04-24 12:50:30 +0200

    Update Listauczniow.java

--- src/Listauczniow.java
+++ src/Listauczniow.java
@@ -1,6 +1,20 @@
+/**
+
+ <klasa Listauczniow zawiera:
+
+w zasadzie to nic
+
+spytasz o pola?
+ i tu jest problem
+
+ @author: <Igor> <Wisniewski>
+
+ **/
+
 public class Listauczniow extends Uczen {
 
 
     public Listauczniow(String imie, String nazwisko) {
         super(imie, nazwisko);
     }
+}
